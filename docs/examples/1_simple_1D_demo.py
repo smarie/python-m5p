e0c650ec462a87bd0bd7bacc7b358e4c596ad4d9
Sylvain MARIE <sylvain.marie@se.com>
2022-04-12 05:01:44 +0200

    Fixed first gallery example

--- docs/examples/1_simple_1D_demo.py
+++ docs/examples/1_simple_1D_demo.py
@@ -57,6 +57,7 @@ print(export_text_m5(regr_3, out_file=None))  # equivalent to as_pretty_text
 
 # %%
 # Plot the results
+fig = plt.figure()
 plt.scatter(X, y, s=20, edgecolor="black",
             c="darkorange", label="data")
 plt.plot(X_test, y_1, color="cornflowerblue", label=regr_1_label, linewidth=2)
@@ -66,3 +67,4 @@ plt.xlabel("data")
 plt.ylabel("target")
 plt.title("Decision Tree Regression")
 plt.legend()
+fig
